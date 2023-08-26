8b486262ac876f41aa7829fb4f33fffa6f1b5d21
Laura Reivinen <laurareivinen@Lauras-MacBook-Air.local>
2023-08-26 16:33:37 +0300

    changed config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -20,6 +21,11 @@ module.exports = {
   "plugins": [
     "react", "jest", "cypress"
   ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
   "rules": {
     "indent": [
       "error",
