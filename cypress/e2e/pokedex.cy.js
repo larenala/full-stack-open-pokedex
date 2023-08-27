f215fcf58b3c418c6430816e7426eb0bd9b314cb
Laura Reivinen <laurareivinen@Lauras-MacBook-Air.local>
2023-08-27 07:10:28 +0300

    changed test and port again

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,13 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5001')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be opened', function() {
-    cy.visit('http://localhost:8080')
-    cy.contains('bulbasaur').click()
-    cy.contains('bulbasaur')
-    cy.contains('special defense')
+    cy.visit('http://localhost:5001')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('weedle').should('not.exist')
   })
 })
