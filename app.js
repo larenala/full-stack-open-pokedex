ee5cf4771274346e3121b60a67dde1a422e5ed5f
Laura Reivinen <laurareivinen@Lauras-MacBook-Air.local>
2023-08-28 19:55:10 +0300

    health check

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/health', (req, res) => {
+  throw Error('Failed instances')
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
